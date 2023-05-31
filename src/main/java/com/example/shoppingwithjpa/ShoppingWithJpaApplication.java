42adbd55e0676a9aab75743b079b367e96c5208f
minbo2002 <minbo2002@naver.com>
2023-05-31 17:19:04 +0900

    feat: 프로젝트 첫 환경설정 세팅

---
+++ src/main/java/com/example/shoppingwithjpa/ShoppingWithJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.shoppingwithjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingWithJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppingWithJpaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/shoppingwithjpa/ShoppingWithJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shoppingwithjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingWithJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
